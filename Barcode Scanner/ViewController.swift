e9e1e4ed1efed8f408ff8cbb0452908c6cd51920
Prakhar Tripathi <prakhar.tripathi@theredpandas.com>
2020-08-16 12:11:27 +0530

    initial commit

---
+++ Barcode Scanner/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Barcode Scanner
+//
+//  Created by Prakhar Tripathi on 16/08/20.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
